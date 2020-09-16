8881a1d90af0d2c9212852793ec48fb4506a2597
Anas Qiblawi <anashgt@gmail.com>
2020-09-16 12:25:53 +0000

    Make 'app' directory.
    and move deleter.js, filemanager.js and uploader.js into it

--- index.js
+++ index.js
@@ -42,15 +42,15 @@ app.get('/', function(req, res) {
 });
 
 // Uploader ---------------------------------
-var uploader = require('./uploader').uploader;
+var uploader = require('./app/uploader').uploader;
 app.post('/uploader', uploader);
 
 // file explorer
-var filemanager = require('./filemanager').filemanager;
+var filemanager = require('./app/filemanager').filemanager;
 app.get('/files', filemanager);
 
 // delete
-var deleter = require('./deleter').deleter;
+var deleter = require('./app/deleter').deleter;
 app.get('/delete/:file_name', deleter);
 
 
