ce03fa1a923dab317d52057c5b5ee1750124bc8f
Anas Qiblawi <anashgt@gmail.com>
2022-05-07 14:43:18 +0300

    Move script location
    move express code from index.js to lib/server.js

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
-// Samples
-// https://www.sample-videos.com/
-// https://video.openedu.tw/Examples/
-//////////////////////////////////////////////////////////////////
-// Tutorials
-// https://expressjs.com/en/guide/routing.html
-// https://www.npmjs.com/package/ejs
-//////////////////////////////////////////////////////////////////
-//---------------------------------
+// Dependencies
+const app = require('./lib/server')
 const fs = require('fs');
-const request = require('request');
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-// ------------------
-//////////////////////////////////////////////////////////////////
-// Setting Server --------------------------------------------------------------
-var port = process.env.PORT || 80;
-app.listen(port, console.log(`Server is Running on port : ${port}`));
-app.set('view engine', 'ejs');
-// Static Requests --------------------------------------------------------------
-app.use('/', express.static(__dirname + '/data'));
-// body-parser Sittings ---------------------------------------------------------
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.raw());
-//app.use(bodyParser.urlencoded());
+
+
+
 //////////////////////////////////////////////////////////////////
 
 // Make a directory for data
